c7055c7a6fc6ae62bd2e48b98b89f80fb8bc78d0
Lienha Carleton <lhariver@gmail.com>
2015-02-22 21:10:29 -0800

    draggable

--- js/sliding-puzzle.js
+++ js/sliding-puzzle.js
@@ -71,5 +71,74 @@
         empty.top = 0;
         empty.left = 0;
         container.find("#ui").find("p").not("#time").remove();
+
+        pieces.draggable({
+            containment: "parent",
+            grid: [pieceW, pieceH],
+            start: function (e, ui) {
+                var current = getPosition(ui.helper);
+                if (current.left === empty.left) {
+                    ui.helper.draggable("option", "axis", "y");
+                } else if (current.top === empty.top) {
+                    ui.helper.draggable("option", "axis", "x");
+                } else {
+                    ui.helper.trigger("mouseup");
+                    return false;
+                }
+
+                if (current.bottom < empty.top ||
+                    current.top > empty.bottom ||
+                    current.left > empty.right ||
+                    current.right < empty.left) {
+
+                    ui.helper.trigger("mouseup");
+                    return false;
+                }
+                previous.top = current.top;
+                previous.left = current.left;
+            },
+            drag: function (e, ui) {
+                var current = getPosition(ui.helper);
+                ui.helper.draggable("option", "revert", false);
+
+                if (current.top === empty.top && current.left === empty.left) {
+                    ui.helper.trigger("mouseup");
+                    return false;
+                }
+
+                if (current.top > empty.bottom ||
+                    current.bottom < empty.top ||
+                    current.left > empty.right ||
+                    current.right < empty.left) {
+
+                    ui.helper.trigger("mouseup").css({
+                        top: previous.top,
+                        left: previous.left
+                    });
+                    return false;
+                }
+            },
+            stop: function (e, ui) {
+                var current = getPosition(ui.helper),
+                    correctPieces = 0;
+
+                if (current.top === empty.top && current.left === empty.left) {
+                    empty.top = previous.top;
+                    empty.left = previous.left;
+                    empty.bottom = previous.top + pieceH;
+                    empty.right = previous.left + pieceW;
+                }
+            }
+        });
+
+        function getPosition(el) {
+            return {
+                top: parseInt(el.css("top")),
+                bottom: parseInt(el.css("top")) + pieceH,
+                left: parseInt(el.css("left")),
+                right: parseInt(el.css("left")) + pieceW
+            }
+        }
+
     });
 })();
